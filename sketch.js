eada21dda908e6641afa91daf76fffcf4d3d35cd
= <=>
2021-04-30 11:10:27 -0300

    Add files

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let mic;
+
+  function setup(){
+    mic = new p5.AudioIn();
+    mic.start();
+  }
+  
+  function draw(){
+      var boca = document.getElementById("path40")
+      var barba =document.getElementById('rect942')
+      var bigodeDireito = document.getElementById("rect930")
+      var bigodeEsquerdo = document.getElementById("rect932")
+      
+      var vol = mic.getLevel()
+  
+      boca.style.ry = vol *180
+      if(boca.style.ry >= "10") {
+        boca.style.ry = "8"
+      }
+      boca.style.rx = 15 - vol*150
+      barba.style.transform = `translateY(${vol*50}px)`
+      bigodeDireito.style.x = -120 + vol*30
+      bigodeEsquerdo.style.x = -120 + vol*30
+  }
+
+  function mousePressed() {
+    userStartAudio();
+  }
